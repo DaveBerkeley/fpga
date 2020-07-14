12b282ab34793d21fb874081ffb52373088ce8f2
Dave Berkeley <via@rotwang.co.uk>
2020-07-14 12:55:10 +0100

    two input audio streams : 4 microphones

--- dsp/firmware.c
+++ dsp/firmware.c
@@ -309,390 +309,6 @@ void cmd_print_spi_state()
         print("OFF\n");
 }
 
-uint32_t xorshift32(uint32_t *state)
-{
-    /* Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" */
-    uint32_t x = *state;
-    x ^= x << 13;
-    x ^= x >> 17;
-    x ^= x << 5;
-    *state = x;
-
-    return x;
-}
-
-void cmd_memtest()
-{
-    int cyc_count = 5;
-    int stride = 256;
-    uint32_t state;
-
-    volatile uint32_t *base_word = (uint32_t *) 0;
-    volatile uint8_t *base_byte = (uint8_t *) 0;
-
-    print("Running memtest ");
-
-    // Walk in stride increments, word access
-    for (int i = 1; i <= cyc_count; i++) {
-        state = i;
-
-        for (int word = 0; word < MEM_TOTAL / sizeof(int); word += stride) {
-            *(base_word + word) = xorshift32(&state);
-        }
-
-        state = i;
-
-        for (int word = 0; word < MEM_TOTAL / sizeof(int); word += stride) {
-            if (*(base_word + word) != xorshift32(&state)) {
-                print(" ***FAILED WORD*** at ");
-                print_hex(4*word, 4);
-                print("\n");
-                return;
-            }
-        }
-
-        print(".");
-    }
-
-    // Byte access
-    for (int byte = 0; byte < 128; byte++) {
-        *(base_byte + byte) = (uint8_t) byte;
-    }
-
-    for (int byte = 0; byte < 128; byte++) {
-        if (*(base_byte + byte) != (uint8_t) byte) {
-            print(" ***FAILED BYTE*** at ");
-            print_hex(byte, 4);
-            print("\n");
-            return;
-        }
-    }
-
-    print(" passed\n");
-}
-
-// --------------------------------------------------------
-
-void cmd_read_flash_id()
-{
-    uint8_t buffer[17] = { 0x9F, /* zeros */ };
-    flashio(buffer, 17, 0);
-
-    for (int i = 1; i <= 16; i++) {
-        putchar(' ');
-        print_hex(buffer[i], 2);
-    }
-    putchar('\n');
-}
-
-// --------------------------------------------------------
-
-#ifdef HX8KDEMO
-uint8_t cmd_read_flash_regs_print(uint32_t addr, const char *name)
-{
-    set_flash_latency(8);
-
-    uint8_t buffer[6] = {0x65, addr >> 16, addr >> 8, addr, 0, 0};
-    flashio(buffer, 6, 0);
-
-    print("0x");
-    print_hex(addr, 6);
-    print(" ");
-    print(name);
-    print(" 0x");
-    print_hex(buffer[5], 2);
-    print("\n");
-
-    return buffer[5];
-}
-
-void cmd_read_flash_regs()
-{
-    print("\n");
-    uint8_t sr1v = cmd_read_flash_regs_print(0x800000, "SR1V");
-    uint8_t sr2v = cmd_read_flash_regs_print(0x800001, "SR2V");
-    uint8_t cr1v = cmd_read_flash_regs_print(0x800002, "CR1V");
-    uint8_t cr2v = cmd_read_flash_regs_print(0x800003, "CR2V");
-    uint8_t cr3v = cmd_read_flash_regs_print(0x800004, "CR3V");
-    uint8_t vdlp = cmd_read_flash_regs_print(0x800005, "VDLP");
-}
-#endif
-
-#ifdef ICEBREAKER
-uint8_t cmd_read_flash_reg(uint8_t cmd)
-{
-    uint8_t buffer[2] = {cmd, 0};
-    flashio(buffer, 2, 0);
-    return buffer[1];
-}
-
-void print_reg_bit(int val, const char *name)
-{
-    for (int i = 0; i < 12; i++) {
-        if (*name == 0)
-            putchar(' ');
-        else
-            putchar(*(name++));
-    }
-
-    putchar(val ? '1' : '0');
-    putchar('\n');
-}
-
-void cmd_read_flash_regs()
-{
-    putchar('\n');
-
-    uint8_t sr1 = cmd_read_flash_reg(0x05);
-    uint8_t sr2 = cmd_read_flash_reg(0x35);
-    uint8_t sr3 = cmd_read_flash_reg(0x15);
-
-    print_reg_bit(sr1 & 0x01, "S0  (BUSY)");
-    print_reg_bit(sr1 & 0x02, "S1  (WEL)");
-    print_reg_bit(sr1 & 0x04, "S2  (BP0)");
-    print_reg_bit(sr1 & 0x08, "S3  (BP1)");
-    print_reg_bit(sr1 & 0x10, "S4  (BP2)");
-    print_reg_bit(sr1 & 0x20, "S5  (TB)");
-    print_reg_bit(sr1 & 0x40, "S6  (SEC)");
-    print_reg_bit(sr1 & 0x80, "S7  (SRP)");
-    putchar('\n');
-
-    print_reg_bit(sr2 & 0x01, "S8  (SRL)");
-    print_reg_bit(sr2 & 0x02, "S9  (QE)");
-    print_reg_bit(sr2 & 0x04, "S10 ----");
-    print_reg_bit(sr2 & 0x08, "S11 (LB1)");
-    print_reg_bit(sr2 & 0x10, "S12 (LB2)");
-    print_reg_bit(sr2 & 0x20, "S13 (LB3)");
-    print_reg_bit(sr2 & 0x40, "S14 (CMP)");
-    print_reg_bit(sr2 & 0x80, "S15 (SUS)");
-    putchar('\n');
-
-    print_reg_bit(sr3 & 0x01, "S16 ----");
-    print_reg_bit(sr3 & 0x02, "S17 ----");
-    print_reg_bit(sr3 & 0x04, "S18 (WPS)");
-    print_reg_bit(sr3 & 0x08, "S19 ----");
-    print_reg_bit(sr3 & 0x10, "S20 ----");
-    print_reg_bit(sr3 & 0x20, "S21 (DRV0)");
-    print_reg_bit(sr3 & 0x40, "S22 (DRV1)");
-    print_reg_bit(sr3 & 0x80, "S23 (HOLD)");
-    putchar('\n');
-}
-#endif
-
-// --------------------------------------------------------
-
-uint32_t cmd_benchmark(bool verbose, uint32_t *instns_p)
-{
-    uint8_t data[256];
-    uint32_t *words = (void*)data;
-
-    uint32_t x32 = 314159265;
-
-    uint32_t cycles_begin, cycles_end;
-    uint32_t instns_begin, instns_end;
-    __asm__ volatile ("rdcycle %0" : "=r"(cycles_begin));
-    __asm__ volatile ("rdinstret %0" : "=r"(instns_begin));
-
-    for (int i = 0; i < 20; i++)
-    {
-        for (int k = 0; k < 256; k++)
-        {
-            x32 ^= x32 << 13;
-            x32 ^= x32 >> 17;
-            x32 ^= x32 << 5;
-            data[k] = x32;
-        }
-
-        for (int k = 0, p = 0; k < 256; k++)
-        {
-            if (data[k])
-                data[p++] = k;
-        }
-
-        for (int k = 0, p = 0; k < 64; k++)
-        {
-            x32 = x32 ^ words[k];
-        }
-    }
-
-    __asm__ volatile ("rdcycle %0" : "=r"(cycles_end));
-    __asm__ volatile ("rdinstret %0" : "=r"(instns_end));
-
-    if (verbose)
-    {
-        print("Cycles: 0x");
-        print_hex(cycles_end - cycles_begin, 8);
-        putchar('\n');
-
-        print("Instns: 0x");
-        print_hex(instns_end - instns_begin, 8);
-        putchar('\n');
-
-        print("Chksum: 0x");
-        print_hex(x32, 8);
-        putchar('\n');
-    }
-
-    if (instns_p)
-        *instns_p = instns_end - instns_begin;
-
-    return cycles_end - cycles_begin;
-}
-
-// --------------------------------------------------------
-
-#ifdef HX8KDEMO
-void cmd_benchmark_all()
-{
-    uint32_t instns = 0;
-
-    print("default        ");
-    reg_spictrl = (reg_spictrl & ~0x00700000) | 0x00000000;
-    print(": ");
-    print_hex(cmd_benchmark(false, &instns), 8);
-    putchar('\n');
-
-    for (int i = 8; i > 0; i--)
-    {
-        print("dspi-");
-        print_dec(i);
-        print("         ");
-
-        set_flash_latency(i);
-        reg_spictrl = (reg_spictrl & ~0x00700000) | 0x00400000;
-
-        print(": ");
-        print_hex(cmd_benchmark(false, &instns), 8);
-        putchar('\n');
-    }
-
-    for (int i = 8; i > 0; i--)
-    {
-        print("dspi-crm-");
-        print_dec(i);
-        print("     ");
-
-        set_flash_latency(i);
-        reg_spictrl = (reg_spictrl & ~0x00700000) | 0x00500000;
-
-        print(": ");
-        print_hex(cmd_benchmark(false, &instns), 8);
-        putchar('\n');
-    }
-
-    for (int i = 8; i > 0; i--)
-    {
-        print("qspi-");
-        print_dec(i);
-        print("         ");
-
-        set_flash_latency(i);
-        reg_spictrl = (reg_spictrl & ~0x00700000) | 0x00200000;
-
-        print(": ");
-        print_hex(cmd_benchmark(false, &instns), 8);
-        putchar('\n');
-    }
-
-    for (int i = 8; i > 0; i--)
-    {
-        print("qspi-crm-");
-        print_dec(i);
-        print("     ");
-
-        set_flash_latency(i);
-        reg_spictrl = (reg_spictrl & ~0x00700000) | 0x00300000;
-
-        print(": ");
-        print_hex(cmd_benchmark(false, &instns), 8);
-        putchar('\n');
-    }
-
-    for (int i = 8; i > 0; i--)
-    {
-        print("qspi-ddr-");
-        print_dec(i);
-        print("     ");
-
-        set_flash_latency(i);
-        reg_spictrl = (reg_spictrl & ~0x00700000) | 0x00600000;
-
-        print(": ");
-        print_hex(cmd_benchmark(false, &instns), 8);
-        putchar('\n');
-    }
-
-    for (int i = 8; i > 0; i--)
-    {
-        print("qspi-ddr-crm-");
-        print_dec(i);
-        print(" ");
-
-        set_flash_latency(i);
-        reg_spictrl = (reg_spictrl & ~0x00700000) | 0x00700000;
-
-        print(": ");
-        print_hex(cmd_benchmark(false, &instns), 8);
-        putchar('\n');
-    }
-
-    print("instns         : ");
-    print_hex(instns, 8);
-    putchar('\n');
-}
-#endif
-
-#ifdef ICEBREAKER
-void cmd_benchmark_all()
-{
-    uint32_t instns = 0;
-
-    print("default   ");
-    set_flash_mode_spi();
-    print_hex(cmd_benchmark(false, &instns), 8);
-    putchar('\n');
-
-    print("dual      ");
-    set_flash_mode_dual();
-    print_hex(cmd_benchmark(false, &instns), 8);
-    putchar('\n');
-
-    // print("dual-crm  ");
-    // enable_flash_crm();
-    // print_hex(cmd_benchmark(false, &instns), 8);
-    // putchar('\n');
-
-    print("quad      ");
-    set_flash_mode_quad();
-    print_hex(cmd_benchmark(false, &instns), 8);
-    putchar('\n');
-
-    print("quad-crm  ");
-    enable_flash_crm();
-    print_hex(cmd_benchmark(false, &instns), 8);
-    putchar('\n');
-
-    print("qddr      ");
-    set_flash_mode_qddr();
-    print_hex(cmd_benchmark(false, &instns), 8);
-    putchar('\n');
-
-    print("qddr-crm  ");
-    enable_flash_crm();
-    print_hex(cmd_benchmark(false, &instns), 8);
-    putchar('\n');
-
-}
-#endif
-
-void cmd_echo()
-{
-    print("Return to menu by sending '!'\n\n");
-    char c;
-    while ((c = getchar()) != '!')
-        putchar(c);
-}
-
     /*
      *
      */
@@ -724,7 +340,7 @@ enum Opcode {
 
 bool verbose = false;
 
-uint32_t opcode(uint8_t opcode, uint8_t offset, uint8_t chan, uint32_t gain)
+uint32_t opcode(uint8_t opcode, uint16_t offset, uint8_t chan, uint32_t gain)
 {
     const uint32_t value = gain + (chan << 16) + (offset << (16+CHAN_W)) + (opcode << (16+CHAN_W+OFFSET_W));
 
@@ -943,6 +559,7 @@ void cmd_dave()
 
     run(opcode(MACZ, 1, 0, 0x1234));
     
+#if 0
     //  Check reading all channels
     print("Writing to audio input\n");
 
@@ -974,6 +591,7 @@ void cmd_dave()
             run(expect);
         }
     }
+#endif
 
     verbose = 0;
     //  Check multiplier input
@@ -1198,6 +816,7 @@ void cmd_dave()
 
     //  Check write output
     verbose = 0;
+#if 0
     print("Check write shift/output\n");
     clr_audio(0);
     set_audio(4 + (1 * FRAMES), 0x1111);
@@ -1248,6 +867,7 @@ void cmd_dave()
             ASSERT(audio == v);
         }
     }
+#endif
 
     //  Check filter performance
     print("Check filter performance\n");
@@ -1276,16 +896,22 @@ void cmd_dave()
     print("Tests run OKAY\n");
     print("==============\n");
 
-    uint32_t atten = 0;
-
     coef = ADDR_COEF;
-    *coef++ = opcode(MACZ, 0, 0, 4);
-    //*coef++ = opcode(MAC,  0, 1, 4);
-    *coef++ = opcode(SAVE, 2 + atten, 0, 0);
 
-    *coef++ = opcode(MACZ, 0, 1, 8);
-    //*coef++ = opcode(MACN, 0, 0, 8);
-    *coef++ = opcode(SAVE, 3 + atten, 0, 1);
+    uint32_t gain = 1024;
+    uint32_t g1_3 = (1 * gain) / 4;
+    uint32_t g2_3 = (1 * gain) / 2;
+    uint32_t shift = 11;
+
+    *coef++ = opcode(MACZ, 0, 1, gain);
+    *coef++ = opcode(MAC,  0, 0, g2_3);
+    *coef++ = opcode(MAC,  0, 3, g1_3);
+    *coef++ = opcode(SAVE, shift, 0, 0);
+
+    *coef++ = opcode(MACZ, 0, 2, gain);
+    *coef++ = opcode(MAC,  0, 3, g2_3);
+    *coef++ = opcode(MAC,  0, 0, g1_3);
+    *coef++ = opcode(SAVE, shift, 0, 1);
 
     *coef++ = halt();
     *coef++ = halt();
@@ -1328,7 +954,6 @@ void main()
     print(" KiB\n");
     print("\n");
 
-    //cmd_memtest(); // test overwrites bss and data memory
     print("\n");
 
     cmd_print_spi_state();
@@ -1337,84 +962,4 @@ void main()
     reg_leds = 0;
 
     cmd_dave();
-
-#if 0
-    while (1)
-    {
-        print("\n");
-
-        print("Select an action:\n");
-        print("\n");
-        print("   [1] Read SPI Flash ID\n");
-        print("   [2] Read SPI Config Regs\n");
-        print("   [3] Switch to default mode\n");
-        print("   [4] Switch to Dual I/O mode\n");
-        print("   [5] Switch to Quad I/O mode\n");
-        print("   [6] Switch to Quad DDR mode\n");
-        print("   [7] Toggle continuous read mode\n");
-        print("   [9] Run simplistic benchmark\n");
-        print("   [0] Benchmark all configs\n");
-        print("   [M] Run Memtest\n");
-        print("   [S] Print SPI state\n");
-        print("   [e] Echo UART\n");
-        print("   [d] Dave Test\n");
-        print("\n");
-
-        for (int rep = 10; rep > 0; rep--)
-        {
-            print("Command> ");
-            char cmd = getchar();
-            if (cmd > 32 && cmd < 127)
-                putchar(cmd);
-            print("\n");
-
-            switch (cmd)
-            {
-            case '1':
-                cmd_read_flash_id();
-                break;
-            case '2':
-                cmd_read_flash_regs();
-                break;
-            case '3':
-                set_flash_mode_spi();
-                break;
-            case '4':
-                set_flash_mode_dual();
-                break;
-            case '5':
-                set_flash_mode_quad();
-                break;
-            case '6':
-                set_flash_mode_qddr();
-                break;
-            case '7':
-                reg_spictrl = reg_spictrl ^ 0x00100000;
-                break;
-            case '9':
-                cmd_benchmark(true, 0);
-                break;
-            case '0':
-                cmd_benchmark_all();
-                break;
-            case 'M':
-                cmd_memtest();
-                break;
-            case 'S':
-                cmd_print_spi_state();
-                break;
-            case 'e':
-                cmd_echo();
-                break;
-            case 'd':
-                cmd_dave();
-                break;
-            default:
-                continue;
-            }
-
-            break;
-        }
-    }
-#endif
 }
