97a44ca645a5443c23cf62470f06e84ce4ae9243
Dave Berkeley <via@rotwang.co.uk>
2020-08-21 12:26:15 +0100

    wip: adding gain stage to AGC

--- dsp/agc.v
+++ dsp/agc.v
@@ -2,19 +2,19 @@
 `default_nettype none
 
 module agc
-#(parameter IN_W=24, CHANS=8, OUT_W=$clog2(IN_W), CHAN_W=$clog2(CHANS))
+#(parameter IN_W=24, OUT_W=16, CHANS=8, LEVEL_W=$clog2(IN_W), CHAN_W=$clog2(CHANS))
 (
     input wire ck,
     input wire en,
-    output reg [CHAN_W-1:0] src_addr,
+    output wire [CHAN_W-1:0] src_addr,
     input wire signed [IN_W-1:0] in_data,
-    output wire [OUT_W-1:0] level,
+    output wire [LEVEL_W-1:0] level,
     output wire done
 );
 
     wire [IN_W-1:0] normal;
 
-    // Take signed input data and convert to absolute value.
+    // Take signed wide input data and convert to absolute value.
     twos_complement #(.WIDTH(IN_W))
     twos_complement(
         .ck(ck),
@@ -24,6 +24,8 @@ module agc
     );
 
     // Calculate the index of the MSB.
+    // This corresponds to the required shift
+    // needed to convert the eg 24-bit signal into a 16-bit one.
     wire level_ready;
     reg level_en = 0;
 
@@ -36,48 +38,127 @@ module agc
         .ready(level_ready)
     );
 
-    initial src_addr = 0;
+    wire find_level;
+    assign find_level = level_en | !level_ready;
 
     reg find_max = 0;
     reg [IN_W-1:0] max_in = 0;
+    reg find_normal = 0;
+    reg [1:0] address = 0;
 
+    wire gain_ready;
+    wire [OUT_W-1:0] gain_out;
+    wire [CHAN_W-1:0] gain_addr;
+    reg gain_en = 0;
+    reg gain_wait = 0;
+
+    wire find_gain;
+    assign find_gain = gain_en | !gain_ready;
+
+    wire busy;
+    assign busy = find_max | find_normal | find_level | find_gain;
+
+    gain #(.IN_W(IN_W), .OUT_W(OUT_W), .CHAN_W(CHAN_W))
+    gain (
+        .ck(ck),
+        .en(gain_en),
+        .addr(gain_addr),
+        .in_data(in_data),
+        .out(gain_out),
+        .done(gain_ready)
+    );
+ 
     always @(posedge ck) begin
+
         if (en) begin
             // Start the sequence
-            src_addr <= 0;
+            address <= 0;
             find_max <= 1;
             max_in <= 0;
         end
 
+        find_normal <= find_max;
+
         if (level_en) begin
             level_en <= 0;
+            gain_wait <= 1;
         end
 
-        if (find_max && !level_en) begin
+        if (gain_wait & level_ready) begin
+            gain_en <= 1;
+            gain_wait <= 0;
+        end
 
+        if (gain_en) begin
+            gain_en <= 0;
+        end
+
+        if (gain_ready & gain_wait) begin
+            gain_wait <= 0;
+        end
+
+        if (find_normal) begin
             // find the largest abs(input)
             if (normal > max_in) begin
                 max_in <= normal;
             end
-
-            if (src_addr != CHANS-1) begin
-                // request the next input
-                src_addr <= src_addr + 1;
-            end else begin
+            if ((address == CHANS-1) && !find_max) begin
                 // Finished level acquisition.
                 // Start to calculate the level
                 level_en <= 1;
+            end
+        end
+
+        // Step through the input channels
+        // looking for the max value.
+        if (find_max && !level_en) begin
+
+            if (address != CHANS-1) begin
+                // request the next input
+                address <= address + 1;
+            end else begin
                 find_max <= 0;
             end
 
         end
     end
 
-    wire busy;
-    assign busy = find_max | level_en | !level_ready;
-
     assign done = !busy;
+    assign src_addr = (find_max ? address : 0) | gain_addr;
 
 endmodule
 
+module gain
+#(parameter IN_W=24, OUT_W=16, CHAN_W=3)
+(
+    input wire ck,
+    input wire en,
+    output reg [CHAN_W-1:0] addr,
+    input wire signed [IN_W-1:0] in_data,
+    output reg [OUT_W-1:0] out,
+    output reg done
+);
+
+    initial done = 0;
+    initial out = 0;
+
+    assign addr = 0;
+
+    reg delay = 0;
+
+    always @(posedge ck) begin
+
+        if (en) begin
+            done <= 0;
+        end
+
+        delay <= en;
+
+        if (delay) begin
+            done <= 1;
+        end
+
+    end
+
+endmodule
 
--- dsp/agc_tb.v
+++ dsp/agc_tb.v
@@ -23,10 +23,10 @@ module tb ();
     wire [23:0] in_data;
     wire done;
 
-    reg [23:0] mic_0 = 24'hZ;
-    reg [23:0] mic_1 = 24'hZ;
-    reg [23:0] mic_2 = 24'hZ;
-    reg [23:0] mic_3 = 24'hZ;
+    reg [23:0] mic_0 = 0;
+    reg [23:0] mic_1 = 0;
+    reg [23:0] mic_2 = 0;
+    reg [23:0] mic_3 = 0;
 
     function [23:0] get_mic(input [1:0] addr);
         begin
@@ -39,7 +39,12 @@ module tb ();
         end
     endfunction
 
-    assign in_data = get_mic(src_addr);
+    assign in_data = (src_addr == 0) ? mic_0 : 
+                     ((src_addr == 1) ? mic_1 :  
+                     ((src_addr == 2) ? mic_2 : 
+                     mic_3));
+
+    wire [4:0] level;
 
     agc #(.IN_W(24), .CHANS(4))
     agc (
@@ -47,6 +52,7 @@ module tb ();
         .en(en),
         .src_addr(src_addr),
         .in_data(in_data),
+        .level(level),
         .done(done)
     );
 
@@ -58,21 +64,110 @@ module tb ();
             @(posedge ck);
         end
     endtask
+
+    integer i;
  
     initial begin
         $display("test agc()");
         @(posedge ck);
         @(posedge ck);
-
-        mic_0 <= 24'h000456;
-        mic_1 <= 24'h010123;
-        mic_2 <= 24'hffe000;
-        mic_3 <= 24'hfbcdef;
-        go();
-        wait(done);
-
         @(posedge ck);
 
+        // Check it runs through the sequence
+        mic_0 <= -1;
+        mic_1 <= -16;
+        mic_2 <= -256;
+        mic_3 <= -1024;
+        go();
+        wait(done);
+        @(posedge ck);
+
+        // Check that all channels are looked at
+        mic_0 <= 24'h000001;
+        mic_1 <= 24'h000000;
+        mic_2 <= 24'h000000;
+        mic_3 <= 24'h000000;
+        @(posedge ck);
+        go();
+        wait(done);
+        tb_assert(level == 22);
+
+        mic_0 <= 24'h000000;
+        mic_1 <= 24'h000001;
+        mic_2 <= 24'h000000;
+        mic_3 <= 24'h000000;
+        @(posedge ck);
+        go();
+        wait(done);
+        tb_assert(level == 22);
+
+        mic_0 <= 24'h000000;
+        mic_1 <= 24'h000000;
+        mic_2 <= 24'h000001;
+        mic_3 <= 24'h000000;
+        @(posedge ck);
+        go();
+        wait(done);
+        tb_assert(level == 22);
+
+        mic_0 <= 24'h000000;
+        mic_1 <= 24'h000000;
+        mic_2 <= 24'h000000;
+        mic_3 <= 24'h000001;
+        @(posedge ck);
+        go();
+        wait(done);
+        tb_assert(level == 22);
+
+        // check each level
+        mic_0 <= 24'h000000;
+        mic_1 <= 24'h000000;
+        mic_2 <= 24'h400000;
+        mic_3 <= 24'h000000;
+        for (i = 0; i < 24; i++) begin
+            @(posedge ck);
+            go();
+            wait(done);
+            @(posedge ck);
+            tb_assert(level == i);
+            mic_2 <= mic_2 >> 1;
+        end
+
+        // check each level for -ve levels
+        mic_0 <= 24'h000000;
+        mic_1 <= 24'h000000;
+        mic_2 <= 24'hbfffff;
+        mic_3 <= 24'h000000;
+        for (i = 0; i < 22; i++) begin
+            @(posedge ck);
+            go();
+            wait(done);
+            @(posedge ck);
+            tb_assert(level == i);
+            mic_2 <= { 1'b1, mic_2[23:1] };
+        end
+
+        // Check mid-word transition, both signs
+        mic_0 <= 24'h000000;
+        mic_1 <= 24'hfff000;
+        mic_2 <= 24'h000000;
+        mic_3 <= 24'h000000;
+        @(posedge ck);
+        go();
+        wait(done);
+        tb_assert(level == 10);
+
+        mic_0 <= 24'h000000;
+        mic_1 <= 24'h000fff;
+        mic_2 <= 24'h000000;
+        mic_3 <= 24'h000000;
+        @(posedge ck);
+        go();
+        wait(done);
+        tb_assert(level == 11);
+
+        @(posedge ck);
+        @(posedge ck);
 
         $finish;
     end
